d4ac020c37ddf1ea3a5d857f64977a276a569da9
wisdom chainx <wisdomchainx111@gmail.com>
2024-10-23 19:05:00 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,123 @@
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId)
+    let nav = document.getElementById(navId)
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+
+showMenu('nav-toggle', 'nav-menu')
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction() {
+    const navMenu = document.getElementById("nav-menu")
+    navMenu.classList.remove("show-menu")
+    console.log(navMenu)
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+// SCROLL SECTION
+
+const sections = document.querySelectorAll('section[id]')
+console.log(sections)
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offset - 50
+        const sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav_menu a[href* = ' + sectionId + ']').classList.add('active-link')
+        }
+        else {
+            document.querySelector('.nav_menu a[href* = ' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+// TO CHANGE BACKGROUND HEADER
+
+function scrollHeader() {
+    const nav = document.getElementById('header')
+    console.log(nav)
+
+    if (this.scrollY >= 200) {
+        nav.classList.add('scroll-header')
+    }
+    else nav.classList.remove('scroll-header')
+}
+
+window.addEventListener('scroll', scrollHeader)
+
+//  TO  SHGOW SCROLL TOP
+
+function scrollTop() {
+    const scrollTop = document.getElementById("scroll-top")
+    console.log(scrollTop)
+
+    if (this.scrollY >= 560) {
+        scrollTop.classList.add('show-scroll')
+    }
+    else {
+        scrollTop.classList.remove('show-scroll')
+    }
+}
+
+window.addEventListener('scroll', scrollTop)
+
+// DARK LIGHT THEME
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme =  'bx-sun'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => {
+    document.body.classList.contains(darkTheme) ? 'light' : 'dark'
+}
+
+const getCurrentIcon = () =>{
+    themeButton.classList.contains(iconTheme) ? 'bx-sun' : 'bx-moon'
+} 
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'] (iconTheme)
+}
+
+themeButton.addEventListener('click', () =>{
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+
+
+
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 2000,
+    reset: true
+});
+
+sr.reveal(`.home__data, .home__img,
+            .about__data, .about__img,
+            .services__content, .menu__content,
+            .app__data, .app__img,
+            .contact__data, .contact__button,
+            .footer__content`, {
+    interval: 200
+})
