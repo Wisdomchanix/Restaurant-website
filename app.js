8dfba76144ea034a18ce0d1ad55e2001cb68509c
wisdom chainx <wisdomchainx111@gmail.com>
2024-10-23 19:09:46 +0100

    add js changes

--- app.js
+++ app.js
@@ -113,11 +113,11 @@ const sr = ScrollReveal({
     reset: true
 });
 
-sr.reveal(`.home__data, .home__img,
-            .about__data, .about__img,
-            .services__content, .menu__content,
-            .app__data, .app__img,
-            .contact__data, .contact__button,
-            .footer__content`, {
+sr.reveal(`.home_data, .home_img,
+            .about_data, .about_img,
+            .services_content, .menu_content,
+            .app_data, .app_img,
+            .contact_data, .contact_button,
+            .footer_content`, {
     interval: 200
 })
